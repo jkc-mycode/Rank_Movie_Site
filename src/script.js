be609532298aaefe4266351451b6743915519fe2
Yun <ydh1503@naver.com>
2024-05-07 14:57:22 +0900

    pull

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// update : 2024 - 05 - 07
+// 작성자 : 윤동협
+
+import { print } from "./movie.js";
+import { makeMovieSlide } from "./movie_slide.js";
+
+// 슬라이드
+await makeMovieSlide();
+
+// 카드
+print();
