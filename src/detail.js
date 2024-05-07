c9de34069742b24d37509d91d5ce66a713127856
jkc-mycode <jkc9709@naver.com>
2024-05-07 23:53:44 +0900

    Update review javascript

--- src/detail.js
+++ src/detail.js
@@ -1,3 +1,5 @@
+import { loadComments, addComment } from "./review.js";
+
 // 조민수 시작포인트
 // TMDB 에서  영화 가져온것
 const options = {
@@ -9,24 +11,6 @@ const options = {
     }
 };
 
-async function getdata() {
-    const response = await fetch("https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1", options);
-    const data = await response.json();
-
-    const newmovieinfo = [];
-
-    for (let item of data["results"]) {
-        const movieinfo = {};
-        movieinfo["title"] = item["title"];
-        movieinfo["overview"] = item["overview"];
-        movieinfo["poster_path"] = item["poster_path"];
-        movieinfo["vote_average"] = item["vote_average"];
-        movieinfo["id"] = item["id"];
-
-        newmovieinfo.push(movieinfo);
-    }
-    return newmovieinfo;
-}
 
 //  영화 감독 출연진 정보에 대한 api가져오기
 async function getcredit(id) {
@@ -58,7 +42,7 @@ let newvideo = function novideo() {
     if (getvideo(id).length > 0) {
         return get[0].key;
     } else if (getvideo(id).length === 0) {
-        return "관련영상이없습니다";
+        return " 관련영상이없습니다";
     }
 };
 
@@ -102,12 +86,46 @@ async function makesubpage() {
         <strong> 출연자 배우</strong>
         <p> ${credits[0][0].name}, ${credits[0][1].name}, ${credits[0][2].name},
         ${credits[0][3].name} </p>
+        <div class="movie_review" id="movie_review">
+            <p><b>리뷰쓰기</b></p>
+            <div class="row g-3">
+                <div class="col-md-4">
+                    <input type="text" id="reviewer" class="form-control" placeholder="작성자" aria-label="작성자">
+                </div>
+                <div class="col-md-4">
+                    <input type="password" id="review_pass" class="form-control" placeholder="비밀번호" aria-label="비밀번호">
+                </div>
+            </div><br>
+            <div class="row g-3">
+                <div class="col-12">
+                    <textarea class="form-control" id="review_content" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 50px"></textarea>
+                </div>
+            </div><br>
+            <input type="hidden" id="movieId" value="영화 아이디를 여기에 입력하세요">
+            <div class="review_btn_box">
+                <button id="review_btn" class="btn btn-primary review_btn" type="button">Submit</button>
+            </div>
+            <div id="commentContainer">
+
+            </div>
+        </div>
     </div>
 </div>
     `;
     document.querySelector("#mainContents").insertAdjacentHTML("beforeend", innerContents);
 }
 
-makesubpage();
+await makesubpage();
+
+// 페이지가 로드될 때 댓글을 불러옴
+loadComments();
+
+// 댓글 작성 양식 제출 시 addComment 함수 호출
+document.getElementById('review_btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    addComment();
+    // 댓글 작성 후 다시 댓글을 불러옴
+    loadComments();
+});
 
 // 조민수 끝나는포인트
